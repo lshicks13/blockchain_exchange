6f7ea01aa247b440b662a8054961140c786208bf
Lauren Hicks <lhickscissp@gmail.com>
2022-05-26 01:02:47 -0400

    added transfer function

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -14,4 +14,13 @@ contract Token {
         totalSupply = 1000000 * (10**decimals);
         balanceOf[msg.sender] = totalSupply;
     }
+
+    function transfer(address _to, uint256 _value)
+        public
+        returns (bool success)
+    {
+        balanceOf[msg.sender] = balanceOf[msg.sender].sub(_value);
+        balanceOf[_to] = balanceOf[_to].add(_value);
+        return true;
+    }
 }
